368fb21c2915008fb2da89a927ade3f7bc2af729
Тимур <blinovtimur72@gmail.com>
2022-12-03 13:02:38 +0200

    Версия 3.

---
+++ character.py
@@ -0,0 +1,11 @@
+class Character:
+    name = ''
+    health = 100
+    damage = 1
+    defence = 0
+
+    def __init__(self, name, health=100, damage=1, defence=0 ):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.defence = defence
--- main.py
+++
@@ -1,2 +0,0 @@
-class Character:
-    def __init__(self):
