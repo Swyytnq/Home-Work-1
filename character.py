ce3c8223df692975fbebef122103adf85661e798
Тимур <blinovtimur72@gmail.com>
2022-12-03 14:44:59 +0200

    Версия 5.

--- character.py
+++ character.py
@@ -10,8 +10,25 @@ class Character:
         self.damage = damage
         self.defence = defence
 
+    def __str__(self):
+        return f'--- {self.name} ---\n' \
+            f'HP: {self.health} \n' \
+            f'Damage: {self.damage} \n' \
+            f'Defence: {self.defence} \n'
+
+
+
+
+
     def print(self):
         print(f'--- {self.name} ---')
         print(f'HP: {self.health}')
         print(f'Damage: {self.damage}')
         print(f'Defence: {self.defence}')
+
+    def take_damage(self, damage):
+        self.health -= damage
+
+    def attack(self, target):
+        target.take_damage(self.damage)
+        print('Attack!')
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from character import Character
 
-player1 = Character('Sigimund')
-player1.print()
+player1 = Character('Sigismund', damage=3)
+player2 = Character('Brundmir', health=35)
+
+
+print(player1)
+print(player2)
+
+player1.attack(player2)
+
+print(player1)
+print(player2)
