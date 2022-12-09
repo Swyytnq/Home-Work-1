b2a9392659424692a0069a904de9d293def1a5c6
Тимур <blinovtimur72@gmail.com>
2022-12-09 21:36:39 +0200

    Версия 6 (домашняя работа).

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from character import Character
+from random import randint
 
-player1 = Character('Sigismund', damage=3)
-player2 = Character('Brundmir', health=35)
+player1 = Character('Sigismund', damage=randint(1, 10), health=randint(50, 100))
+player2 = Character('Brundmir', damage=randint(1, 10), health=randint(50, 100))
 
-
-print(player1)
-print(player2)
-
-player1.attack(player2)
 
 print(player1)
 print(player2)
+
+while player1.health > 0 and player2.health > 0:
+    player1.attack(player2)
+    player2.attack(player1)
+    print('=================')
+    print(player1)
+    print(player2)
